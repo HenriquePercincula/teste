48f75cb63d2fee0f8c3045bb455f092909832bd7
HenriquePercincula <36556383+HenriquePercincula@users.noreply.github.com>
2019-03-22 11:22:10 -0300

    linha 5 - alteração css
    justiificar o texto do p1

--- trabalho.js
+++ trabalho.js
@@ -1,8 +1,8 @@
-function inserir(){
-    var body = querySelector("body");
+function inserir(){   
     var p = document.createElement("p");
     p.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     p.style.fontFamily='Verdana';
+    p.style.textAlign="justify";
     var div = document.querySelector("#div1");
     var btn = document.querySelector("button");
     document.body.insertBefore(p, btn.nextSibling);
